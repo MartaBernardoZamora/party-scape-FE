f63cfe24f4a82871cdbb3bcce931f5527127d769
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-23 15:39:13 +0200

    DeleteMatch flow working

--- src/components/matches/MatchesTable.jsx
+++ src/components/matches/MatchesTable.jsx
@@ -2,31 +2,12 @@ import { useEffect, useState } from 'react'
 import Button from '../utils/Button';
 
 function MatchesTable(props) {
-    const [matches, setMatches] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const adminId = 1; // Harcoded admin ID
+    if (!props.matches) return <p>No se pudo cargar la tabla.</p>;
 
-    useEffect(() => {
-        const fetchMatches = async () => {
-            try {
-                const response = await fetch(`http://localhost:8080/api/v1/admin/${adminId}/matches?lobbyId=${props.lobbyId}`);
-                const data = await response.json();
-                setMatches(data);
-            } catch (error) {
-                console.error('Error al cargar la sala:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchMatches();
-    }, [props.lobbyId]);
-
-    if (loading) return <p>Cargando tabla...</p>;
-    if (!matches) return <p>No se pudo cargar la tabla.</p>;
 
     return (
         <>
-            {matches.length === 0 ? (
+            {props.matches.length === 0 ? (
                 <p>No hay partidas en esta sala.</p>
             ) : (
                 <table border="1">
@@ -39,14 +20,14 @@ function MatchesTable(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {matches.map((match) => (
+                        {props.matches.map((match) => (
                             <tr key={match.id}>
                                 <td>{match.id}</td>
                                 <td>{match.joinCode}</td>
                                 <td>{match.status}</td>
                                 <td>
                                     <Button text="👀" onClick={() => console.log('Ver datos')} title="Ver datos" disabled={match.status !== 'FINISHED'}/>
-                                    <Button text="❌" onClick={() => console.log('Eliminar partida')} title="Eliminar partida" />
+                                    <Button text="❌" onClick={() => props.onDeleteMatch(match.id)} title="Eliminar partida" />
                                 </td>
                             </tr>
                         ))}
--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -7,29 +7,49 @@ function Matches() {
     const { lobbyId } = useParams();
     const [lobby, setLobby] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [matches, setMatches] = useState([]);
     const adminId = 1; // Harcoded admin ID
 
     useEffect(() => {
         const fetchLobby = async () => {
             try {
-            const response = await fetch(`http://localhost:8080/api/v1/admin/${adminId}/lobbies/${lobbyId}`);
-            const data = await response.json();
-            setLobby(data);
+                const response = await fetch(`http://localhost:8080/api/v1/admin/${adminId}/lobbies/${lobbyId}`);
+                const data = await response.json();
+                setLobby(data);
+
+                const matchesResponse = await fetch(`http://localhost:8080/api/v1/admin/${adminId}/matches?lobbyId=${lobbyId}`);
+                const matchesData = await matchesResponse.json();
+                setMatches(matchesData);
             } catch (error) {
-            console.error('Error al cargar la sala:', error);
+                console.error('Error al cargar la sala o las partidas:', error);
             } finally {
-            setLoading(false);
+                setLoading(false);
             }
         };
         fetchLobby();
-    }, [lobbyId]);
+    }, [lobbyId, matches]);
+    
+
+    const handleDeleteMatch = async (matchId) => {
+        const confirmacion = window.confirm('¿Estas seguro de que quieres eliminar esta partida?');
+        if (confirmacion) {
+            try {
+                await fetch(`http://localhost:8080/api/v1/admin/${adminId}/matches/${matchId}`, {
+                    method: 'DELETE',
+                });
+                setMatches(matches.filter((match) => match.id !== matchId));
+            } catch (error) {
+                console.error('Error al eliminar la partida:', error);
+            }
+        }
+    };
 
     if (loading) return <p>Cargando sala...</p>;
     if (!lobby) return <p>No se pudo cargar la sala.</p>;
     return (
         <>
             <h1>Partidas de la sala {lobby.name}</h1>
-            <MatchesTable lobbyId={lobby.id}/>
+            <MatchesTable matches = {matches} onDeleteMatch={handleDeleteMatch}/>
         </>
     )
 }
