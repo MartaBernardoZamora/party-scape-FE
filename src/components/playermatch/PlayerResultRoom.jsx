04722c93105d04594a2275bbebfa68817d9cdee0
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-27 17:52:16 +0200

    Refatory to show total time

--- src/components/playermatch/PlayerResultRoom.jsx
+++ src/components/playermatch/PlayerResultRoom.jsx
@@ -4,6 +4,8 @@ import { useWebSocketContext } from '../../contexts/WebSocketContext';
 function PlayerResultRoom({ matchId, finalTime }) {
         const { connect, send, close } = useWebSocketContext();
         const [results, setResults] = useState([]);
+        const [match, setMatch] = useState(null);
+        const adminId = 1; //Harcodeado
       
         useEffect(() => {
           const socket = connect(
@@ -19,24 +21,43 @@ function PlayerResultRoom({ matchId, finalTime }) {
             () => {
               send({ type: "GET_RESULTS" });
             }
-          );
+          );         
       
-          return () => {
+        const fetchMatch = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/v1/admin/${adminId}/matches/${matchId}`);
+                const data = await response.json();
+                setMatch(data);
+            } catch (error) {
+                console.error('Error al cargar la partida:', error);
+            } 
+        }
+        fetchMatch();
+        return () => {
             close();
           };
         }, [matchId, connect, send, close]);
+        const calculateTimeSpent = (start, end) => {
+            const startTime = new Date(start).getTime();
+            const endTime = new Date(end).getTime();
+            const totalSeconds = Math.floor((endTime - startTime) / 1000);
+        
+            const minutes = Math.floor(totalSeconds / 60);
+            const seconds = totalSeconds % 60;
+        
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+          };
 
   return (
     <>
-      <p>Tu tiempo final: {finalTime.toLocaleTimeString()}</p>
-      <h1>Resultados de la partida</h1>
-      <ul>
+      <h1>Clasificación de la partida</h1>
+      <ol>
         {results.map((r, index) => (
           <li key={index}>
-            {r.playerName} - {new Date(r.finalTime).toLocaleTimeString()}
+            {r.playerName} - {calculateTimeSpent(match.startDatetime, r.finalTime)}
           </li>
         ))}
-      </ul>
+      </ol>
     </>
   )
 }
