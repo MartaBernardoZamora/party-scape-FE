3e65cc7a2b0164307c4883ff4c862fa022eb351f
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-06-11 10:03:43 +0200

    feat: github actions workflow created

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/party-scape-FE/',
   plugins: [react(), mkcert()],
   server: {
     https: true
